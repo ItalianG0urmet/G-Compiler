e2354481accf37422447b4700c8feed47f1b4bb5
gourmet <insogna_@outlook.it>
2025-07-26 01:42:22 +0200

    Syntax fix

--- src/main.c
+++ src/main.c
@@ -7,7 +7,6 @@ int main(int argc, const char* argv[]) {
 
     struct Flags flags = get_arguments_flags(argc, argv);
     compile_file(flags.file_name, flags.output_name, flags.debug_active);
-    return 0;
 
     return 0;
 }
