379c2b6eb7cbcd4e7cc171d271fd33eae632758b
ItalianG0urmet <insogna_@outlook.it>
2025-08-14 14:03:40 +0200

    Added color support

--- src/error_utils.c
+++ src/error_utils.c
@@ -1,23 +1,46 @@
 #include "error_utils.h"
 
+#include <stdbool.h>
 #include <stdio.h>
+
 #include "lexer.h"
+
+#ifdef _WIN32
+#include <io.h>
+#define isatty _isatty
+#define fileno _fileno
+#else
+#include <unistd.h>
+#endif
+
+static const char* RED;
+static const char* RESET;
+
+static void init_color_support(void) {
+    bool use_colors = isatty(fileno(stderr));
+    RED = use_colors ? "\033[31m" : "";
+    RESET = use_colors ? "\033[0m" : "";
+}
 
 void send_syntax_error_by_line(const char* line, const int column,
                                const char* error) {
+    init_color_support();
+
     fprintf(stderr,
-            "\n\x1b[31mError\x1b[0m at line %d:\n"
+            "\n%sError%s at column %d:\n"
             "  %s\n"
             "  %s\n",
-            column, error, line);
+            RED, RESET, column, error, line);
 }
 
 void send_syntax_error(const struct Token* token, const char* error) {
     if (!token) return;
 
+    init_color_support();
+
     fprintf(stderr,
-            "\n\x1b[31mError\x1b[0m at line %d:\n"
+            "\n%sError%s at column %d:\n"
             "  %s\n"
             "  %s\n",
-            token->column, error, token->full_line);
+            RED, RESET, token->column, error, token->full_line);
 }
